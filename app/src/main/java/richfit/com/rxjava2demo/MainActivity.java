4254b74a40b5005d4af189ea0bfbd32a05eb68b1
monday <upcyff@126.com>
2016-10-25 19:21:18 +0800

    Initial commit

---
+++ app/src/main/java/richfit/com/rxjava2demo/MainActivity.java
@@ -0,0 +1,13 @@
+package richfit.com.rxjava2demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
